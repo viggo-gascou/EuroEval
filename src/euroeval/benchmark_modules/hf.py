87dc134ffd9828db426882ece7b791bc6b9464db
Dan Saattrup Nielsen <47701536+saattrupdan@users.noreply.github.com>
2025-03-20 12:30:20 +0100

    Fix/reasoning token detection (#856)
    * debug
    
    * fix: Disallow using BOS token in prompt as beginning-of-reasoning token
    
    * fix: Ignore if TASK_MAPPING does not have pipeline tag

--- src/euroeval/benchmark_modules/hf.py
+++ src/euroeval/benchmark_modules/hf.py
@@ -802,7 +802,7 @@ def get_model_repo_info(
         generative_class_names = [
             class_name
             for tag in GENERATIVE_PIPELINE_TAGS
-            for class_name in TASK_MAPPING[tag].values()
+            for class_name in TASK_MAPPING.get(tag, dict()).values()
         ]
         if class_names is not None and any(
             class_name in generative_class_names for class_name in class_names
