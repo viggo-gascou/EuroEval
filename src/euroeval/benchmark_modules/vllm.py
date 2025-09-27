f4abf5ac8ca67e3fd224544402400685ff38f564
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-09-27 08:53:08 +0200

    style: Imports

--- src/euroeval/benchmark_modules/vllm.py
+++ src/euroeval/benchmark_modules/vllm.py
@@ -15,9 +15,9 @@ import torch
 from huggingface_hub import snapshot_download
 from pydantic import conlist, create_model
 from tqdm.auto import tqdm
-from transformers import MistralCommonTokenizer
 from transformers.models.auto.configuration_auto import AutoConfig
 from transformers.models.auto.tokenization_auto import AutoTokenizer
+from transformers.tokenization_mistral_common import MistralCommonTokenizer
 from urllib3.exceptions import RequestError
 
 from ..constants import (
