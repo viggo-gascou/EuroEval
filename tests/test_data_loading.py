447a679954377a8ea5534f70318b1f2798bbdeba
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-10-01 17:20:37 +0200

    tests: Add test that checks that RC datasets has an 'id' col

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -16,6 +16,7 @@ from euroeval.data_models import BenchmarkConfig, DatasetConfig
 from euroeval.dataset_configs import get_all_dataset_configs, get_dataset_config
 from euroeval.enums import GenerativeType
 from euroeval.generation_utils import apply_prompt, extract_few_shot_examples
+from euroeval.tasks import RC
 
 
 @pytest.fixture(scope="module")
@@ -66,78 +67,97 @@ class TestLoadData:
 
 @pytest.mark.parametrize(
     argnames="dataset_config",
-    argvalues=list(get_all_dataset_configs().values()),
-    ids=list(get_all_dataset_configs().keys()),
+    argvalues=[
+        dataset_config
+        for dataset_config in get_all_dataset_configs().values()
+        if os.getenv("CHECK_DATASET") is not None
+        and (
+            dataset_config.name in os.environ["CHECK_DATASET"].split(",")
+            or any(
+                language.code in os.environ["CHECK_DATASET"].split(",")
+                for language in dataset_config.languages
+            )
+            or "all" in os.environ["CHECK_DATASET"].split(",")
+        )
+    ],
+    ids=lambda dc: dc.name,
 )
-def test_examples_in_official_datasets_are_not_too_long(
-    dataset_config: DatasetConfig, benchmark_config: BenchmarkConfig, tokeniser_id: str
-) -> None:
-    """Test that the examples are not too long in official datasets."""
-    # Skip unless we've manually chosen to run this test with a given dataset, since it
-    # is quite slow to run on all datasets
-    if os.getenv("CHECK_DATASET") is None:
-        pytest.skip(
-            reason="Skipping test for all datasets, as no dataset was explicitly "
-            "requested with the `CHECK_DATASET` environment variable."
+class TestAllDatasets:
+    """Tests that are run on all datasets."""
+
+    def test_examples_in_official_datasets_are_not_too_long(
+        self,
+        dataset_config: DatasetConfig,
+        benchmark_config: BenchmarkConfig,
+        tokeniser_id: str,
+    ) -> None:
+        """Test that the examples are not too long in official datasets."""
+        dummy_model_config = LiteLLMModel.get_model_config(
+            model_id="model", benchmark_config=benchmark_config
         )
-    datasets_to_check = [
-        dataset_name.strip() for dataset_name in os.environ["CHECK_DATASET"].split(",")
-    ]
-    if dataset_config.name not in datasets_to_check and all(
-        language.code not in datasets_to_check for language in dataset_config.languages
-    ):
-        pytest.skip(
-            reason=f"Skipping test for dataset {dataset_config.name!r}, as it was not "
-            "explicitly requested with the `CHECK_DATASET` environment variable."
+        tokeniser = AutoTokenizer.from_pretrained(tokeniser_id)
+        dataset = load_raw_data(
+            dataset_config=dataset_config, cache_dir=benchmark_config.cache_dir
         )
 
-    dummy_model_config = LiteLLMModel.get_model_config(
-        model_id="model", benchmark_config=benchmark_config
-    )
-    tokeniser = AutoTokenizer.from_pretrained(tokeniser_id)
-    dataset = load_raw_data(
-        dataset_config=dataset_config, cache_dir=benchmark_config.cache_dir
-    )
-
-    for itr_idx in range(10):
-        few_shot_examples = (
-            extract_few_shot_examples(
-                dataset=dataset,
-                dataset_config=dataset_config,
-                benchmark_config=benchmark_config,
-                itr_idx=itr_idx,
-            )
-            if not dataset_config.task.requires_zero_shot
-            else []
-        )
-        for instruction_model in [True, False]:
-            prepared_test = dataset["test"].map(
-                partial(
-                    apply_prompt,
-                    few_shot_examples=few_shot_examples,
-                    model_config=dummy_model_config,
+        for itr_idx in range(10):
+            few_shot_examples = (
+                extract_few_shot_examples(
+                    dataset=dataset,
                     dataset_config=dataset_config,
-                    generative_type=(
-                        GenerativeType.INSTRUCTION_TUNED
-                        if instruction_model
-                        else GenerativeType.BASE
+                    benchmark_config=benchmark_config,
+                    itr_idx=itr_idx,
+                )
+                if not dataset_config.task.requires_zero_shot
+                else []
+            )
+            for instruction_model in [True, False]:
+                prepared_test = dataset["test"].map(
+                    partial(
+                        apply_prompt,
+                        few_shot_examples=few_shot_examples,
+                        model_config=dummy_model_config,
+                        dataset_config=dataset_config,
+                        generative_type=(
+                            GenerativeType.INSTRUCTION_TUNED
+                            if instruction_model
+                            else GenerativeType.BASE
+                        ),
+                        always_populate_text_field=True,
+                        tokeniser=tokeniser,
                     ),
-                    always_populate_text_field=True,
-                    tokeniser=tokeniser,
-                ),
-                batched=True,
-                load_from_cache_file=False,
-                keep_in_memory=True,
-            )
+                    batched=True,
+                    load_from_cache_file=False,
+                    keep_in_memory=True,
+                )
 
-            max_input_length = max(
-                len(tokeniser(prompt)["input_ids"]) for prompt in prepared_test["text"]
-            )
-            max_output_length = dataset_config.max_generated_tokens
-            max_length = max_input_length + max_output_length
+                max_input_length = max(
+                    len(tokeniser(prompt)["input_ids"])
+                    for prompt in prepared_test["text"]
+                )
+                max_output_length = dataset_config.max_generated_tokens
+                max_length = max_input_length + max_output_length
 
-            assert max_length <= MAX_CONTEXT_LENGTH, (
-                f"Max length of {max_length:,} exceeds the maximum context length "
-                f"({MAX_CONTEXT_LENGTH:,}) for dataset {dataset_config.name} in "
-                f"iteration {itr_idx} and when instruction_model={instruction_model}."
+                assert max_length <= MAX_CONTEXT_LENGTH, (
+                    f"Max length of {max_length:,} exceeds the maximum context length "
+                    f"({MAX_CONTEXT_LENGTH:,}) for dataset {dataset_config.name} in "
+                    f"iteration {itr_idx} and when instruction_model="
+                    f"{instruction_model}."
+                )
+
+    def test_reading_comprehension_datasets_have_id_column(
+        self, dataset_config: DatasetConfig, benchmark_config: BenchmarkConfig
+    ) -> None:
+        """Test that reading comprehension datasets have an ID column."""
+        # Skip if the dataset is not a reading comprehension dataset
+        if dataset_config.task != RC:
+            pytest.skip(reason="Skipping test for non-reading comprehension dataset.")
+
+        dataset = load_raw_data(
+            dataset_config=dataset_config, cache_dir=benchmark_config.cache_dir
+        )
+        for split in dataset_config.splits:
+            assert "id" in dataset[split].features, (
+                f"Dataset {dataset_config.name} is a reading comprehension dataset but "
+                f"the {split} split does not have an 'id' column."
             )
