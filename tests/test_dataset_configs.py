8ca4208639042c41336cab04d66d9d8dd9f2a27c
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-10-20 16:08:47 +0200

    tests: Add test checking for duplicate dataset config variable names

--- tests/test_dataset_configs.py
+++ tests/test_dataset_configs.py
@@ -1,9 +1,11 @@
 """Tests for the `dataset_configs` module."""
 
+from collections import defaultdict
 from typing import Generator
 
 import pytest
 
+from euroeval import dataset_configs as dc_module
 from euroeval.data_models import DatasetConfig
 from euroeval.dataset_configs import get_all_dataset_configs, get_dataset_config
 
@@ -42,3 +44,37 @@ class TestGetDatasetConfig:
         """Test that an error is raised when the dataset does not exist."""
         with pytest.raises(ValueError):
             get_dataset_config("does-not-exist")
+
+
+def test_no_duplicate_dataset_config_variable_names() -> None:
+    """Test that there are no duplicate variable names for dataset configs."""
+    # Create a mapping from language name to list of variable names for the dataset
+    # configs of that language
+    submodules = [
+        value
+        for value in dc_module.__dict__.values()
+        if isinstance(value, type(dc_module))
+    ]
+    language_to_dataset_vars: dict[str, list[str]] = {
+        submodule.__name__.split(".")[-1]: [
+            var_name
+            for var_name, var_value in submodule.__dict__.items()
+            if isinstance(var_value, DatasetConfig)
+        ]
+        for submodule in submodules
+    }
+
+    # Count the number of occurences of each dataset config variable name
+    dataset_variable_name_counts: dict[str, int] = defaultdict(int)
+    for var_names in language_to_dataset_vars.values():
+        for var_name in var_names:
+            dataset_variable_name_counts[var_name] += 1
+
+    # Raise an error if any variable name occurs more than once
+    duplicate_variable_names = [
+        name for name, count in dataset_variable_name_counts.items() if count > 1
+    ]
+    assert not duplicate_variable_names, (
+        f"Duplicate dataset config variable names found: {duplicate_variable_names}. "
+        "Please ensure that each dataset config variable has a unique name."
+    )
