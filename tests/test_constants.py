103561e98ef35dee83a04f236cadbe0616b79f8c
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-11-11 13:57:34 +0100

    tests: Include 're' case in test_all_objects_in_constants_are_constants

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -9,7 +9,7 @@ from euroeval.data_models import Task
 def test_all_objects_in_constants_are_constants() -> None:
     """Test that all objects in the `constants` module are constants."""
     for name in dir(constants):
-        if name.startswith("__") or name in {"TypeVar", "TaskGroup"}:
+        if name.startswith("__") or name in {"TypeVar", "TaskGroup", "re"}:
             continue
         assert name.isupper() and isinstance(
             getattr(constants, name), (Task, TypeVar, int, float, str, list, dict)
