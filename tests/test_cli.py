8b922f666c34759df91b9988d8128814dcea1bf4
Dan Saattrup Nielsen <dan.nielsen@alexandra.dk>
2025-12-29 19:23:57 +0100

    tests: Remove test that checks types of CLI args

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 """Tests for the `cli` module."""
 
-from click.types import BOOL, FLOAT, INT, STRING, Choice, ParamType, Path
+from click.types import ParamType
 
 
 def test_cli_param_names(cli_params: dict[str, ParamType]) -> None:
@@ -9,8 +9,8 @@ def test_cli_param_names(cli_params: dict[str, ParamType]) -> None:
         "model",
         "task",
         "language",
-        "finetuning_batch_size",
         "dataset",
+        "finetuning_batch_size",
         "progress_bar",
         "raise_errors",
         "verbose",
@@ -27,44 +27,13 @@ def test_cli_param_names(cli_params: dict[str, ParamType]) -> None:
         "api_base",
         "api_version",
         "gpu_memory_utilization",
-        "debug",
-        "help",
         "requires_safetensors",
         "generative_type",
         "custom_datasets_file",
         "download_only",
+        "debug",
         "model_language",
         "dataset_language",
         "batch_size",
+        "help",
     }
-
-
-def test_cli_param_types(cli_params: dict[str, ParamType]) -> None:
-    """Test that the CLI parameters have the correct types."""
-    assert cli_params["model"] == STRING
-    assert cli_params["dataset"] == STRING
-    assert isinstance(cli_params["language"], Choice)
-    assert cli_params["task"] == STRING
-    assert isinstance(cli_params["finetuning_batch_size"], Choice)
-    assert cli_params["progress_bar"] == BOOL
-    assert cli_params["raise_errors"] == BOOL
-    assert cli_params["verbose"] == BOOL
-    assert cli_params["save_results"] == BOOL
-    assert cli_params["cache_dir"] == STRING
-    assert cli_params["api_key"] == STRING
-    assert cli_params["force"] == BOOL
-    assert isinstance(cli_params["device"], Choice)
-    assert cli_params["trust_remote_code"] == BOOL
-    assert cli_params["clear_model_cache"] == BOOL
-    assert cli_params["evaluate_test_split"] == BOOL
-    assert cli_params["few_shot"] == BOOL
-    assert cli_params["num_iterations"] == INT
-    assert cli_params["api_base"] == STRING
-    assert cli_params["api_version"] == STRING
-    assert cli_params["gpu_memory_utilization"] == FLOAT
-    assert cli_params["debug"] == BOOL
-    assert cli_params["help"] == BOOL
-    assert cli_params["requires_safetensors"] == BOOL
-    assert isinstance(cli_params["generative_type"], Choice)
-    assert isinstance(cli_params["custom_datasets_file"], Path)
-    assert cli_params["download_only"] == BOOL
